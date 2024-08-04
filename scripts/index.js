50118e154aa87d30a41db7823624eae22dbc7dc9
Daniil Evdokimov <daniil.evdokimov2000@yandex.ru>
2024-08-04 19:41:31 +0300

    Function of adding and removing cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
-// @todo: Темплейт карточки
+import { initialCards } from './cards.js';
 
-// @todo: DOM узлы
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: Функция создания карточки
+// DOM узлы
+const cardsContainer = document.querySelector('.places__list')
 
-// @todo: Функция удаления карточки
+// Функция создания карточки
+const createCard = (name, link, onDeleteCard) => {
+    const cardElement = cardTemplate.querySelector('.places__item.card').cloneNode(true);
 
-// @todo: Вывести карточки на страницу
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__title').textContent = name;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button')
+    deleteButton.addEventListener("click", onDeleteCard);
+
+    return cardElement
+}
+
+// Функция удаления карточки
+const handleDeleteCard = (evt) => {
+    evt.target.closest(".card").remove();
+};
+
+// Вывод карточек на страницу
+initialCards.forEach(card => {
+    const cardElement = createCard(card.name, card.link, handleDeleteCard);
+    cardsContainer.appendChild(cardElement);
+});
